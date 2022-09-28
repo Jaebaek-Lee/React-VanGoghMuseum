b377e788397c42a4b374d4b7caf73f251b516493
Lee Jae-Baek <103738589+ThinkingVincent@users.noreply.github.com>
2022-09-28 19:01:10 +0900

    깜빡하고 푸시 안했던 커밋
    덕분에 올커밋은 물건너감 ㅠㅠ

--- App.js
+++ App.js
@@ -1,22 +1,35 @@
 import './App.css';
+import React, {useState} from 'react';
 
-function Header() {
+function Header(props) {
   return(
-    <h1><a href = "/">Thinking Vincent</a></h1>
-
+    <h1><a href = "/" className = "animated" onClick={event => {
+      event.preventDefault();
+      props.onChangeMode();
+      document.querySelector('body').style.backgroundColor = '#1A1B28';
+      document.querySelector('a').style.color = 'white';
+    }}>{props.title}</a></h1>
   );
 }
 
+function Nav(props) {
+  const list = [];
+  return(
+    <ul>
+      {props.list}
+    </ul>
+  )
+}
+
 function App() {
-  let a = 1
-  const change = ()=> {
-    a = a+1;
-    console.log(a);
+  const [mode, setMode] = useState('THINKING VINCENT');
+  if (mode === 'THINKING VINCENT') {
+    
   }
   return (
-    <div className="App" onClick={change}>
-      <Header ></Header>
-      {a}
+    <div className="App">
+      <Header title = "Thinking Vincent" onChangeMode = {() => {setMode('THINKING VINCENT');}}></Header>
+      <Nav></Nav>
     </div>
   );
 }
